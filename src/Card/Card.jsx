d4ab4abb0979459ff4814a942584bec71b04ce4a
JudeaDumont <dumontjudea2@gmail.com>
2022-12-16 03:16:23 -0700

    react app initial changes

---
+++ src/Card/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card() {
+    return (
+        <div>
+            <img
+                style={{ width: "15rem" }}
+                src="https://images.unsplash.com/photo-1581299894007-aaa50297cf16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                alt="chef"
+            ></img>
+            <div>
+                <p>chef</p>
+
+            </div>
+        </div>
+    )
+}
